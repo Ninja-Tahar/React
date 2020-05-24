3498b205e992c6411e9c3aa3aac7b54c660bdfb6
Benkara Mohamed Tahar <tahar.benkara@ninjacoders-nc.com>
2020-05-25 00:01:50 +0300

    video-3. Other if condition stracture on the same file.

--- src/App.js
+++ src/App.js
@@ -51,36 +51,9 @@ class App extends Component {
 
         let persons = null;
 
-        // if ( this.state.showPersons ) {
-        //     persons = (
-        //         <div>
-        //             <Person
-        //                 name={this.state.persons[0].name}
-        //                 age={this.state.persons[0].age} />
-        //             <Person
-        //                 name={this.state.persons[1].name}
-        //                 age={this.state.persons[1].age}
-        //                 click={this.switchNameHandler.bind( this, 'Mohamed Tahar!' )}
-        //                 changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-        //             <Person
-        //                 name={this.state.persons[2].name}
-        //                 age={this.state.persons[2].age} />
-        //         </div>
-        //     );
-        // }
-
-        return (
-            <div className="App">
-                <h1>Hi, I'm a React App</h1>
-                <p>This is really working!</p>
-                <button
-                    style={style}
-                    onClick={this.togglePersonsHandler}>Toggle Persons</button>
-                {/*{persons}*/}
-
-                {
-                    this.state.showPersons  ?
-                    <div>
+        if ( this.state.showPersons ) {
+            persons = (
+                <div>
                     <Person
                         name={this.state.persons[0].name}
                         age={this.state.persons[0].age} />
@@ -92,7 +65,19 @@ class App extends Component {
                     <Person
                         name={this.state.persons[2].name}
                         age={this.state.persons[2].age} />
-                </div> : null}
+                </div>
+            );
+        }
+
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+                <button
+                    style={style}
+                    onClick={this.togglePersonsHandler}>Toggle Persons</button>
+                {persons}
+
             </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
